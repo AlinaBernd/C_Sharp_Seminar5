534912b15136c1ad97dea88bfa6844c6563cf738
alina bernd <a-manaeva@mail.ru>
2023-08-09 01:09:43 +0300

    Добавила решение задачи 34

--- Program.cs
+++ Program.cs
@@ -1,9 +1,55 @@
 ﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
 // [345, 897, 568, 234] -> 2
+int[] CreateRandomArray(int size, int minValue, int maxValue)
+{
+    int[] array = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        int num = new Random().Next(minValue, maxValue + 1);
+        while (num < 100 || num > 999) //  проверка, чтобы цифры были трехзначные
+        {
+        num = new Random().Next(minValue, maxValue + 1);
+        }
+    array[i] = num;
+    }
+      return array;
+}
+void WriteArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        Console.Write(array[i] + " ");
+
+    Console.WriteLine();
+}
+
+int SumOfOddNumbers(int[] array)
+{
+    int sum = 0;
+
+    for (int i = 0; i < array.Length; i++)
+        if (array[i] % 2 == 0) // отбирает четные числа в массиве
+            sum += array[i];
+
+    return sum;
+}
+
+Console.Write("Input a length of array: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a min possible value: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a max possible value: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+int[] myArray = CreateRandomArray(m, a, b);
+WriteArray(myArray);
+Console.WriteLine(SumOfOddNumbers(myArray));
+
 
 // Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
 // [3, 7, 23, 12] -> 19
 // [-4, -6, 89, 6] -> 0
+/*
 int[] CreateRandomArray(int size, int minValue, int maxValue)
 {
     int[] array = new int[size];
@@ -26,7 +72,7 @@ int SumOfOddNumbers(int[] array)
     int sum = 0;
 
     for (int i = 0; i < array.Length; i++)
-        if (array[i]%2 != 0)
+        if (array[i]%2 != 0) // отбирает нечетные числа в массиве
             sum += array[i];
 
     return sum;
@@ -42,6 +88,7 @@ int b = Convert.ToInt32(Console.ReadLine());
 int[] myArray = CreateRandomArray(m, a, b);
 WriteArray(myArray);
 Console.WriteLine(SumOfOddNumbers(myArray));
+*/
 
 // Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
 // [3.22, 4.2, 1.15, 77.15, 65.2] => 77.15 - 1.15 = 76
